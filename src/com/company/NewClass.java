6b86328acd185962b31196a820c31092b5a17d84
Pavlentiy76 <guk_zekohi@mail.ru>
2020-08-18 20:23:32 +0300

    add new class

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -1,10 +1,9 @@
 package com.company;
 
-public class Main {
+public class NewClass {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world");
-
+        System.out.println("Hello in new class");
     }
+
 }
